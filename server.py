4bacc396d8505af2153b491d9dd9b112a66cadc4
Ryan <ryanzhang0112@gmail.com>
2017-04-22 17:11:44 -0400

    Server Edits

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from multiprocessing import *
 from collections import deque
 from numpy import *
 import pickle
+import glob
 
 players = {}
 playernumber = 1
@@ -18,6 +19,16 @@ PlayerUUID = {}
 sendQueue = Queue()
 messageQueue = Queue()
 itemLib = {}
+'''
+while True:
+    command = input("Do you want to create a new world?[Y/N]:").lower()
+    #generate_world(world_seed, maxHeight, minX, maxX, w, h)
+    #world = generate_world(input("Seed:\n"), 1, 3, 10, 10000, 100)
+    if command == 'y':
+        seed = input("Enter a seed[Don't enter a seed to randomize]: ")
+        try:
+            height = int(input("Enter the max world height[Default]: "))
+'''
 
 class Player(object):
     global PlayerData, PlayerUUID, itemLib
@@ -27,7 +38,7 @@ class Player(object):
         self.UUID = self.get_UUID()
         self.number = PlayerNumber
 
-        self.x, self.y, self.spawnx, self.spawny, self.inventory, self.health, self.hunger, self.satura = self.get_playerInfo()
+        self.cord, self.spawnCord, self.inventory, self.health, self.hunger, self.satura = self.get_playerInfo()
 
     def get_UUID(self):
         try:
@@ -40,11 +51,11 @@ class Player(object):
         try:
             return PlayerData[self.UUID]
         except:
-            PlayerData[self.UUID] = [(), [[0] * 2] * 36, 10, 10, 10]
+            PlayerData[self.UUID] = [(0,0), (0,0), [[0] * 2 for _ in range(36)], 10, 10, 10]
             return PlayerData[self.UUID]
 
-    def changeLocation(self, cord):
-        self.x, self.y = cord[0], cord[1]
+    def changeLocation(self, cordx):
+        self.cord = cordx[:]
 
     def changeInventory(self, item, slot, amount):
         self.inventory[slot][0] = self.itemLib[item]
@@ -74,7 +85,7 @@ class Player(object):
 
     def save(self):
         self.PlayerData[self.UUID] = [(self.x, self.y), self.inventory, self.health, self.hunger, self.satura]
-
+'''
 class World:
     def __init__(self):
         self.overworld = self.loadworld()
@@ -87,7 +98,7 @@ class World:
 
     def placeblock(self, x, y, blocktype):
         slef.overworld[x, y] = blocktype
-
+'''
 def playerSender(sendQueue, server):
     print('Sender running...')
 
@@ -101,19 +112,20 @@ def recieveMessage(messageQueue, server):
 
     while True:
         msg = server.recvfrom(1024)
-        messageQueue.put((msg))
+        messageQueue.put((pickle.loads(msg[0]),msg[1]))
+        print(messageQueue)
 
 
 if __name__ == '__main__':
     host = "127.0.0.1"
     port = 4909
 
-    world = World()
+    #world = World()
 
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind((host, port))
 
-    reciever = Process(target=sendAllPlayer, args=(messageQueue, server))
+    reciever = Process(target=recieveMessage, args=(messageQueue, server))
     reciever.start()
 
     sender = Process(target=playerSender, args=(sendQueue,server))
@@ -122,6 +134,7 @@ if __name__ == '__main__':
     while True:
         pickledmessage = messageQueue.get()
         message, address = pickledmessage
+        print(message, address)
         command = message[0]
 
         if command == 0:
@@ -134,12 +147,14 @@ if __name__ == '__main__':
             else:
                 PN = playerNDisconnect.popleft()
 
-            players[address] = (Player(PN, message[1]), message[2])
+            players[address] = (Player(PN, message[0]), message[1])
+            print('Connection established!')
 
         elif command == 1:
             # Player movement
             # Data: [1, <cordx>, <cordy>]
-            players[address][1].changelocation((message[1], message[2]))
+            players[address][0].changeLocation((message[1], message[2]))
+            print(address, message[1], message[2])
 
         elif command == 2:
             # Render world
