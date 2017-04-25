4a7f6ca62c33e25cc86df8c678374a9c87dab67d
Ryan <ryanzhang0112@gmail.com>
2017-04-24 21:35:09 -0400

    Server and generation update

---
+++ Server tester.py
@@ -0,0 +1,44 @@
+import pickle
+import socket
+from multiprocessing import *
+
+sendQueue = Queue()
+
+def playerSender(sendQueue, server):
+    print('Client running...')
+
+    while True:
+        tobesent = sendQueue.get()
+        server.sendto(pickle.dumps(tobesent[0], protocol=4), tobesent[1])
+
+
+def recieveMessage(server):
+    print('Client is ready for connection!')
+
+    while True:
+        msg = server.recvfrom(1024)
+        print((pickle.loads(msg[0]), msg[1]))
+
+
+if __name__ == '__main__':
+    with open("config", "r") as config:
+        config = config.read().split("\n")
+        host = config[0]
+        port = int(config[1])
+        worldname = config[2]
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((host, port))
+
+    print("Server binded to %s:%i" % (host, port))
+
+    reciever = Process(target=recieveMessage, args=server)
+    reciever.start()
+
+    sender = Process(target=playerSender, args=(sendQueue, server))
+    sender.start()
+    sendQueue.put(pickle.dumps([0, 'Henry']))
+
+    while True:
+        msg = input("Enter command seperated by space: ")
+        sendQueue.put((msg.split(), (host, port)))
