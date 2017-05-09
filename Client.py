96ab377a5dcf3f03d638ba93f4789d483ae9d4ad
henry <henryt.inc@gmail.com>
2017-05-08 23:05:55 -0400

    More menu stuff

--- Client.py
+++ Client.py
@@ -5,7 +5,89 @@ import numpy as np
 import socket
 import rahma
 import menu
+import Game
+import Connection
 import pickle
+
+
+def login():
+    pass
+
+
+def about():
+    pass
+
+
+def options():
+    pass
+
+
+def assistance():
+    clock = time.Clock()
+
+    wallpaper = transform.scale(image.load("textures/menu/wallpaper.png"), (955, 500))
+    screen.blit(wallpaper, (0, 0))
+
+    back_button = menu.Button(200, 370, 400, 40, 'menu', "Back")
+
+    normal_font = font.Font("fonts/minecraft.ttf", 14)
+
+    about_list = ['HELP',
+                  '------------------------------------',
+                  'BOIII',
+                  'SO YOU WANNA PLAY DIS GAME HUH?',
+                  'WELL ITS RLLY EZ ACTUALLY',
+                  'LEGIT',
+                  'YOU TAKE UR FAT FINGERS',
+                  'PRESS DOWN',
+                  'ON UR KEYBOARD',
+                  'AND UR DONE.',
+                  'DO U SEE THAT PERIOD????',
+                  'IT MEANS *MIC DROP*',
+                  '',
+                  'THATS RIGHT',
+                  'ANYWAYS, GOD SAVE THE QUEEN',
+                  'LONG LIVE THE RAHMISH EMPIRE',
+                  '']
+
+    while True:
+
+        click = False
+        release = False
+
+        for e in event.get():
+            if e.type == QUIT:
+                return 'exit'
+
+            if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                click = True
+
+            if e.type == MOUSEBUTTONUP and e.button == 1:
+                release = True
+
+        mx, my = mouse.get_pos()
+        mb = mouse.get_pressed()
+
+        for y in range(0, len(about_list)):
+            about_text = normal_font.render(about_list[y], True, (255, 255, 255))
+            screen.blit(about_text, (400 - about_text.get_width() // 2, 50 + y * 20))
+
+        nav_update = back_button.update(screen, mx, my, mb, 15, release)
+
+        if nav_update is not None:
+            return nav_update
+
+        clock.tick(120)
+        display.update()
+
+
+def server_picker():
+    pass
+
+
+def custom_server_picker():
+    pass
+
 
 def menu_screen():
     clock = time.Clock()
@@ -61,6 +143,7 @@ def menu_screen():
         hover_over_button = False
 
         nav_update = main_menu.update(screen, release, mx, my, mb)
+
         if nav_update:
             return nav_update
 
@@ -108,21 +191,21 @@ if __name__ == "__main__":
 
     click_cursor_data = ((24, 24), (7, 1), *cursors.compile(click_cursor))
     mouse.set_cursor(*cursors.tri_left)
-    """
+
     UI = {'login': login,
-          'menu': menu,
+          'menu': menu_screen,
           'about': about,
           'options': options,
           'assistance': assistance,
-          'game': game,
+          'game': Game.game,
           'server_picker': server_picker,
           'custom_server_picker': custom_server_picker,
-          'local_server': local_server,
-          'rahmish_server': rahmish_server}
-    """
+          'local_server': Connection.local_server,
+          'rahmish_server': Connection.rahmish_server}
+
 
     while navigation != 'exit':
-        navigation = menu_screen()  # UI[navigation]()
+        navigation = UI[navigation]()
 
     display.quit()
     raise SystemExit
