277b6e3dd0ae9cc3ff763b45ccd2b17f0ad78bdc
Syed <syed.safwaan@gmail.com>
2017-05-27 00:33:07 -0400

    Physics update (implemented in game) *NO X-ACCEL YET*

--- components/player.py
+++ components/player.py
@@ -6,28 +6,33 @@ font.init()
 
 normal_font = font.Font("fonts/minecraft.ttf", 14)
 
-class Player:
-    def __init__(self, x, y, w, h, g_cap, reach, controls):
 
+class Player:
+    def __init__(self, x, y, w, h, cap, reach, controls):
         self.rect = Rect(x, y, w, h)
-        self.surfSize = (800, 500)
+
+        self.actual_x = x
+        self.actual_y = y
 
         self.vx = 0
         self.vy = 0
 
-        self.run_speed = int(self.rect.w * 0.3)
-        self.jump_height = -(self.rect.h // 2)
-        self.gravity = self.rect.h * 2 / 45
-        self.grav_cap = g_cap
+        self.vx_inc = 0
+        self.vy_inc = 0.5
+
+        self.base_vy = -(cap / 10 + 2.25)
+
+        self.max_vx = cap // 10
+        self.max_vy = cap
+
+        self.vfly = cap
+
+        # self.run_speed = int(self.rect.w * 0.3)
+        # self.jump_height = -(self.rect.h // 2)
+        # self.gravity = self.rect.h * 2 / 45
+        # self.grav_cap = g_cap
 
         self.reach = reach
-
-        # Lighting
-        # draw.circle(self.reach_surf, Color(255, 255, 255, (reach * 20 - a) * 2), (reach * 20, reach * 20), a)
-
-        # self.reach_surf = Surface((reach * 40, reach * 40), SRCALPHA)
-        # for a in range(reach * 10, 0, -5):
-        #     draw.circle(self.reach_surf, Color(255, 255, 255, a), (reach * 20, reach * 20), a * 2)
 
         self.controls = controls
         self.standing = False
@@ -35,19 +40,26 @@ class Player:
         self.surrounding_shifts = [(x, y) for x in range(-2, 3) for y in range(-2, 4)]
 
     def control(self, fly):
-        if key.get_pressed()[self.controls[0]]:
-            self.vx = -self.run_speed
-        if key.get_pressed()[self.controls[1]]:
-            self.vx = self.run_speed
 
         if fly:
-            if key.get_pressed()[self.controls[2]] or key.get_pressed()[self.controls[4]]:
-                self.vy = -self.run_speed
-            if key.get_pressed()[self.controls[3]]:
-                self.vy = self.run_speed
+            if key.get_pressed()[self.controls[0]]:
+                self.vx = -self.vfly
+            if key.get_pressed()[self.controls[1]]:
+                self.vx = self.vfly
 
-        if (key.get_pressed()[self.controls[2]] or key.get_pressed()[self.controls[4]]) and self.standing:
-            self.vy = self.jump_height
+            if key.get_pressed()[self.controls[2]] or key.get_pressed()[self.controls[4]]:
+                self.vy = -self.vfly
+            if key.get_pressed()[self.controls[3]]:
+                self.vy = self.vfly
+
+        else:
+            if key.get_pressed()[self.controls[0]]:
+                self.vx = -self.max_vx
+            if key.get_pressed()[self.controls[1]]:
+                self.vx = self.max_vx
+
+            if (key.get_pressed()[self.controls[2]] or key.get_pressed()[self.controls[4]]) and self.standing:
+                self.vy = self.base_vy
 
         self.standing = False
 
@@ -67,7 +79,7 @@ class Player:
         if fly:
             self.vy = 0
         else:
-            self.vy += self.gravity if self.vy + self.gravity < self.grav_cap else 0
+            self.vy += self.vy_inc if self.vy + self.vy_inc < self.max_vy else 0
 
         self.rect.x += self.vx
 
@@ -80,46 +92,6 @@ class Player:
 
         self.vx = 0
 
-        # self.rect.y += int(self.vy)
-        # self.rect.x += int(self.vx)
-        #
-        # bumped = False
-        #
-        # for block in blocks:
-        #     if self.rect.colliderect(block):
-        #         if self.vy > 0:
-        #             self.rect.bottom = block.top
-        #             self.standing = True
-        #
-        #         elif self.vy < 0:
-        #             self.rect.top = block.bottom
-        #
-        #         self.vy = 0
-        #         bumped = True
-        #
-        # if fly:
-        #     self.gravity = 0
-        # else:
-        #     self.gravity = self.rect.h * 2 / 45
-        #
-        # for block in blocks:
-        #     if self.rect.colliderect(block):
-        #         if self.vx > 0:
-        #             self.rect.right = block.left
-        #
-        #         elif self.vx < 0:
-        #             self.rect.left = block.right
-        #
-        # self.vx = 0
-        #
-        # if fly:
-        #     self.vy = 0
-        #
-        # if self.vy + self.gravity < self.rect.h and not bumped:
-        #     self.vy += self.gravity
-        # else:
-        #     self.vy -= 0
-
     def detect(self, world, block_size, block_clip, block_properties):
         surrounding_blocks = []
 
@@ -128,7 +100,9 @@ class Player:
                 block_clip[1] - y_shift * block_size) // block_size]
 
             if block_id == -1 or block_properties[block_id][6] == 'collide':
-                surrounding_blocks.append(Rect(block_clip[0] - x_shift * block_size, block_clip[1] - y_shift * block_size, block_size, block_size))
+                surrounding_blocks.append(
+                    Rect(block_clip[0] - x_shift * block_size, block_clip[1] - y_shift * block_size, block_size,
+                         block_size))
 
         return surrounding_blocks
 
@@ -140,17 +114,6 @@ class Player:
 
         self.collide(collision_blocks, fly)
 
-        # triangle = [(self.rect.x - x_offset, self.rect.y - y_offset + self.rect.h // 2),
-        #             (self.rect.x - x_offset + self.rect.w//2, self.rect.y - y_offset),
-        #             (self.rect.x - x_offset + self.rect.w, self.rect.y - y_offset + self.rect.h // 2)]
-        #
-        # draw.polygon(surf, (255, 255, 255), triangle)
-        # draw.rect(surf, (255, 255, 255), (self.rect.x - x_offset, self.rect.y - y_offset + self.rect.h//2, self.rect.w, self.rect.h//2))
-        # center = (self.rect.x - x_offset + self.rect.w // 2, self.rect.y - y_offset + self.rect.h // 2)
-        # draw.circle(surf, (0, 0, 0), center, reach * 20, 3)
-
-        # surf.blit(self.reach_surf,
-        #           ((self.rect.centerx - self.reach * 20) - x_offset, (self.rect.centery - self.reach * 20) - y_offset))
         draw.rect(surf, (255, 255, 255), (self.rect.x - x_offset, self.rect.y - y_offset, self.rect.w, self.rect.h))
 
 
@@ -191,3 +154,24 @@ class RemotePlayer:
                                              self.name_back.get_width(), self.name_back.get_height()))
         surf.blit(self.name_tag, rah.center(self.x - 10 - x_offset, self.y - 40 - y_offset, 20, 20,
                                             self.name_tag.get_width(), self.name_tag.get_height()))
+
+
+# Lighting
+# draw.circle(self.reach_surf, Color(255, 255, 255, (reach * 20 - a) * 2), (reach * 20, reach * 20), a)
+
+# self.reach_surf = Surface((reach * 40, reach * 40), SRCALPHA)
+# for a in range(reach * 10, 0, -5):
+#     draw.circle(self.reach_surf, Color(255, 255, 255, a), (reach * 20, reach * 20), a * 2)
+
+# KKK model
+# triangle = [(self.rect.x - x_offset, self.rect.y - y_offset + self.rect.h // 2),
+#             (self.rect.x - x_offset + self.rect.w//2, self.rect.y - y_offset),
+#             (self.rect.x - x_offset + self.rect.w, self.rect.y - y_offset + self.rect.h // 2)]
+#
+# draw.polygon(surf, (255, 255, 255), triangle)
+# draw.rect(surf, (255, 255, 255), (self.rect.x - x_offset, self.rect.y - y_offset + self.rect.h//2, self.rect.w, self.rect.h//2))
+# center = (self.rect.x - x_offset + self.rect.w // 2, self.rect.y - y_offset + self.rect.h // 2)
+# draw.circle(surf, (0, 0, 0), center, reach * 20, 3)
+
+# surf.blit(self.reach_surf,
+#           ((self.rect.centerx - self.reach * 20) - x_offset, (self.rect.centery - self.reach * 20) - y_offset))
