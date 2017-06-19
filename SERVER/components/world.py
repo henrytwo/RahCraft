08a20da719d57f811bd528c4674bbb331c609405
henry <henryt.inc@gmail.com>
2017-06-19 08:16:42 -0400

    World generation bug fix

--- SERVER/components/world.py
+++ SERVER/components/world.py
@@ -59,107 +59,114 @@ def generate_structure(bx, by, world, structure):
 def generate_world(world_seed, biome_min, biome_max, w, h):
     """ Creates a world object randomly generated using a user-inputted seed. """
 
-    # Set the initial seed for the random module (random.seed())
-    seed(world_seed)
+    while True:
 
-    # Create a blank map (2D list filled with '0' strings
-    world = [[0 for y in range(h)] for x in range(w)]
-    # Generates the random values for the terrain construction
-    terrain = [randrange(20) + 40 for _ in range(w)]
+        try:
 
-    #Empty biome map
-    biomes = []
+            # Set the initial seed for the random module (random.seed())
+            seed(world_seed)
 
-    #Generates biomes
-    for __ in range(w//biome_min):
+            # Create a blank map (2D list filled with '0' strings
+            world = [[0 for y in range(h)] for x in range(w)]
+            # Generates the random values for the terrain construction
+            terrain = [randrange(20) + 40 for _ in range(w)]
 
-        #Biome at cursor
-        biome_select = choice(list(biome_data))
+            #Empty biome map
+            biomes = []
 
-        #Biomes size
-        for _ in range(randint(biome_min, biome_max)):
-            biomes.append(biome_select)
+            #Generates biomes
+            for __ in range(w//biome_min):
 
-        #World size met
-        if len(biomes) >= w:
-            biomes = biomes[:w] #Truncate selection
-            break
+                #Biome at cursor
+                biome_select = choice(list(biome_data))
+
+                #Biomes size
+                for _ in range(randint(biome_min, biome_max)):
+                    biomes.append(biome_select)
+
+                #World size met
+                if len(biomes) >= w:
+                    biomes = biomes[:w] #Truncate selection
+                    break
 
 
-    # ----- Construct the Terrain
-    # Counter that changes dynamically to check through all blocks in the terrain list
-    cur_pos = 0
-    # Runs through all the generated numbers in a while loop
-    while cur_pos < w:
+            # ----- Construct the Terrain
+            # Counter that changes dynamically to check through all blocks in the terrain list
+            cur_pos = 0
+            # Runs through all the generated numbers in a while loop
+            while cur_pos < w:
 
-        # print(".", end="")
+                # print(".", end="")
 
-        # Check to see if terrain gap is too large
+                # Check to see if terrain gap is too large
 
-        if abs(terrain[cur_pos] - terrain[cur_pos - 1]) > biome_data[str(biomes[cur_pos])]["maxh"]:  # if terrain gap is larger than threshhold (too big)
+                if abs(terrain[cur_pos] - terrain[cur_pos - 1]) > biome_data[str(biomes[cur_pos])]["maxh"]:  # if terrain gap is larger than threshhold (too big)
 
-            for n in range(randint(biome_data[str(str(biomes[cur_pos]))]["minx"], biome_data[str(str(biomes[cur_pos]))]["maxx"])):
-                # Insert a new value into the terrain list between the values that are too far apart
-                terrain.insert(cur_pos, (terrain[cur_pos] + terrain[cur_pos - 1]) // 2)
+                    for n in range(randint(biome_data[str(str(biomes[cur_pos]))]["minx"], biome_data[str(str(biomes[cur_pos]))]["maxx"])):
+                        # Insert a new value into the terrain list between the values that are too far apart
+                        terrain.insert(cur_pos, (terrain[cur_pos] + terrain[cur_pos - 1]) // 2)
 
-        else:  # Difference between the two blocks is not too big
+                else:  # Difference between the two blocks is not too big
 
-            # Check next block
-            cur_pos += 1
+                    # Check next block
+                    cur_pos += 1
 
-    # ----- Transfer Terrain To Empty World
-    # Run through every space in the empty world
-    for x in range(len(world)):  # runs through each level
-        for y in range(len(world[x])):  # runs through each individual space
+            # ----- Transfer Terrain To Empty World
+            # Run through every space in the empty world
+            for x in range(len(world)):  # runs through each level
+                for y in range(len(world[x])):  # runs through each individual space
 
-            # Generates structures
-            if y > terrain[x]:
+                    # Generates structures
+                    if y > terrain[x]:
 
-                #Top layer
-                if y - terrain[x] == 1:
+                        #Top layer
+                        if y - terrain[x] == 1:
 
-                    #Sets the layer with block specified in biome config
-                    world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["top"]]
+                            #Sets the layer with block specified in biome config
+                            world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["top"]]
 
-                    if randint(0, 10) == 0 and x + 10 < w:
-                        world = generate_structure(x, y - 1, world, choice(biome_data[biomes[x]]["structure"]))
+                            if randint(0, 10) == 0 and x + 10 < w:
+                                world = generate_structure(x, y - 1, world, choice(biome_data[biomes[x]]["structure"]))
 
-                #Middle layer
-                elif y - terrain[x] < randint(3, 8):
-                    world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["middle"]]
+                        #Middle layer
+                        elif y - terrain[x] < randint(3, 8):
+                            world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["middle"]]
 
-                #Base
-                else:
-                    world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["lower"]]
+                        #Base
+                        else:
+                            world[x][y] = block_lookup[biome_data[biomes[x]]["layer"]["lower"]]
 
-                #Generate ores
-                # Coal
-                if 10 + terrain[x] > y > 5 + terrain[x] and randint(0, 200) == 0:
-                    for cluster in range(randint(3, 10)):
-                        world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Coal Ore"]
+                        #Generate ores
+                        # Coal
+                        if 10 + terrain[x] > y > 5 + terrain[x] and randint(0, 200) == 0:
+                            for cluster in range(randint(3, 10)):
+                                world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Coal Ore"]
 
-                # Iron
-                if 30 + terrain[x] > y > 20 + terrain[x] and randint(0, 200) == 0:
+                        # Iron
+                        if 30 + terrain[x] > y > 20 + terrain[x] and randint(0, 200) == 0:
 
-                    for cluster in range(randint(3, 6)):
-                        world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Iron Ore"]
+                            for cluster in range(randint(3, 6)):
+                                world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Iron Ore"]
 
-                # Gold
-                if 80 > y > 65 and randint(0, 400) == 0:
-                    for cluster in range(randint(3, 6)):
-                        world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Gold Ore"]
+                        # Gold
+                        if 80 > y > 65 and randint(0, 400) == 0:
+                            for cluster in range(randint(3, 6)):
+                                world[x + randint(-4, 4)][y + randint(-4, 4)] = block_lookup["Gold Ore"]
 
-                # Diamonds
-                if 80 > y > 70 and randint(0, 500) == 0:
-                    for cluster in range(randint(1, 5)):
-                        world[x + randint(-3, 3)][y + randint(-3, 3)] = block_lookup["Diamond Ore"]
+                        # Diamonds
+                        if 80 > y > 70 and randint(0, 500) == 0:
+                            for cluster in range(randint(1, 5)):
+                                world[x + randint(-3, 3)][y + randint(-3, 3)] = block_lookup["Diamond Ore"]
 
-                # Bedrock
-                if y > 92 or y > 87 and randint(0, 3) == 0:
-                    world[x][y] = block_lookup["Bed Rock"]
+                        # Bedrock
+                        if y > 92 or y > 87 and randint(0, 3) == 0:
+                            world[x][y] = block_lookup["Bed Rock"]
 
-    # Last edit, adding extras to the top of the world to prevent problems
-    world = [[0] * 40 + x for x in world]
+            # Last edit, adding extras to the top of the world to prevent problems
+            world = [[0] * 40 + x for x in world]
 
-    # Return the world object for use
-    return np.array(world)
+            # Return the world object for use
+            return np.array(world)
+
+        except:
+            world_seed += '1'
